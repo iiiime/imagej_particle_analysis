128eb1a6941ed2b3c3820fc06ba947e9fab330b9
ziw <zijnuw@gmail.com>
2018-04-20 10:50:05 -0700

    write to file updated

--- dalao.py
+++ dalao.py
@@ -2,9 +2,9 @@ from ij import IJ
 from ij.measure import ResultsTable
 import os, sys
 
-filepath = "/Users/inchOuOsis/test" #替换为总目录地址
+filepath = "filepath" # 替换为总目录地址
 dirs = os.listdir(filepath)
-f = open('/Users/inchOuOsis/Documents/output.csv', 'w') #替换为保存地址
+f = open('saving file path', 'w') # 替换为保存地址
 IJ.run("Set Measurements...", "area mean min integrated redirect=None decimal=3")
 
 for files in dirs:
@@ -17,17 +17,18 @@ for files in dirs:
 	imp.close()
 	IJ.setThreshold(10000, 65535)
 	IJ.run("Analyze Particles...", "size=0-Infinity circularity=0-1.00 show=Nothing display clear")
-	IJ.saveAs("Results", "/Users/inchOuOsis/Documents/Results.csv")
+	IJ.saveAs("Results", "saving file path/Results.csv") # 替换为保存地址
 	IJ.run("Close")
 
 	column = [[] for i in range(7)]
-	with open('/Users/inchOuOsis/Documents/Results.csv', 'r') as read_file:
+	with open('saving file path/Results.csv', 'r') as read_file: # 替换为保存地址
 		next(read_file)
 		for row in read_file:
 			a = row.strip().split(',')
 			for i in range(0, len(a)):
 				column[i].append(float(a[i]))
 
+	f.write(str(files))
 	f.write(str(sum(column[1])) + ',')
 	f.write(str((sum(column[2])/len(column[2]))) + ',')
 	f.write(str(min(column[3])) + ',')
