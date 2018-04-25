685de7397a8299c70a34f24e4a7e6fb3a9ae1d15
ziw <zijnuw@gmail.com>
2018-04-24 19:52:54 -0700

    fixed issues on single channel tirf images

--- particles.py
+++ particles.py
@@ -1,8 +1,8 @@
-from ij import IJ
-from ij.measure import ResultsTable
+from ij import IJ, ImageStack
+#from ij.measure import ResultsTable
 import os, sys
 
-filepath = "filepath" # 替换为总目录地址
+filepath = "filpath" # 替换为总目录地址
 dirs = os.listdir(filepath)
 f = open(filepath + '/output.csv', 'w')
 f.write('Label,Area,Mean,Min,Max,IntDen,RawIntDen\n')
@@ -17,16 +17,20 @@ for files in dirs:
 
   img = IJ.open(filepath + '/' + str(files) + '/' + str(files) + suffix +
                 ".tif")
-  IJ.run("Split Channels")
-  imp = IJ.getImage()
-  imp.close()
+
+  img = IJ.getImage()
+  if img.getNSlices() > 1:
+    IJ.run("Split Channels")
+    img = IJ.getImage()
+    img.close()
+  
   IJ.setThreshold(10000, 65535) # 设置threshold
   IJ.run("Analyze Particles...", "size=0-Infinity circularity=0-1.00 show=Nothing display clear")
-  IJ.saveAs("Results", filepath + "/Results.csv")
+  IJ.saveAs("Results", filepath + '/' + str(files) + '/' + "/Results.csv")
   IJ.run("Close")
 
   column = [[] for i in range(7)]
-  with open(filepath + '/Results.csv', 'r') as read_file:
+  with open(filepath + '/' + str(files) + '/' + '/Results.csv', 'r') as read_file:
     next(read_file)
     for row in read_file:
       a = row.strip().split(',')
